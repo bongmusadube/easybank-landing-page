b31c1a46268f7b812d0e3d7f16654c6915214037
Musa Dube <bongmusadube1@gmail.com>
2020-08-26 14:33:03 +0200

    mobile version of the site is complete but no perfect and subject to changes

--- script/main.js
+++ script/main.js
@@ -1,19 +1,18 @@
-
 let menu = document.querySelector(".menu-icon");
 let menuIcon = document.querySelector(".menu-icon img");
 let mobileNav = document.querySelector(".mobile-nav ul");
-
-
-menu.addEventListener('click', () =>
-{
-    menuIcon.classList.toggle('menu-open');
-    if (menuIcon.classList.contains("menu-open"))
-    {
-        mobileNav.style.visibility = "visible";
-        menuIcon.src = "./images/icon-close.svg"
-    }
-    else {
-        mobileNav.style.visibility = "hidden";
-        menuIcon.src = "./images/icon-hamburger.svg"
-    }
+let mockUp = document.querySelector(".mock-up");
+console.log(mockUp);
+menu.addEventListener("click", () => {
+  menuIcon.classList.toggle("menu-open");
+  if (menuIcon.classList.contains("menu-open")) {
+    mobileNav.style.visibility = "visible";
+    menuIcon.src = "./images/icon-close.svg";
+    mockUp.style.visibility = "hidden";
+  } else {
+    mobileNav.style.visibility = "hidden";
+    menuIcon.src = "./images/icon-hamburger.svg";
+    mockUp.style.visibility = "visible";
+  }
+   
 });
